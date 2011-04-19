3f556a4a8a31a4b26c9278c556f78307fca32965
James Walker <walkah@walkah.net>
2011-04-19 18:03:38 -0400

    as cute as that was, it seems to cause issues

--- walkah/javascript.el
+++ walkah/javascript.el
@@ -3,10 +3,3 @@
 (add-to-list 'auto-mode-alist '("\\.json$" . espresso-mode))
 (setq espresso-indent-level 2)
 
-
-(eval-after-load 'espresso
-  '(progn (font-lock-add-keywords
-           'espresso-mode `(("\\(function *\\)("
-                             (0 (progn (compose-region (match-beginning 1)
-                                                       (match-end 1) "ƒ")
-                                       nil)))))))
