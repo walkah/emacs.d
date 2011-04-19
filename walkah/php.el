8e6195fec79769e233f3a9778a577e906dce3759
James Walker <walkah@walkah.net>
2011-04-19 18:10:33 -0400

    flymake php setup

--- walkah/php.el
+++ walkah/php.el
@@ -45,8 +45,28 @@
    (setq truncate-lines t)
    (define-key c-mode-map "\C-m" 'newline-and-indent)
 
+   (flymake-mode 1)
+
    ; code sniffer - to assure coding standards compliance
    (set (make-local-variable 'compile-command)
         (format "phpcs --report=emacs --standard=PEAR %s"
                 (buffer-file-name))))
 (add-hook 'php-mode-hook 'walkah/php-mode-init)
+
+(require 'flymake)
+(defun flymake-php-init ()
+  "Use php to check the syntax of the current file."
+  (let* ((temp (flymake-init-create-temp-buffer-copy 'flymake-create-temp-inplace))
+	 (local (file-relative-name temp (file-name-directory buffer-file-name))))
+    (list "php" (list "-f" local "-l"))))
+
+(add-to-list 'flymake-err-line-patterns
+  '("\\(Parse\\|Fatal\\) error: +\\(.*?\\) in \\(.*?\\) on line \\([0-9]+\\)$" 3 4 nil 2))
+
+(add-to-list 'flymake-allowed-file-name-masks '("\\.php$" flymake-php-init))
+
+;; Drupal-type extensions
+(add-to-list 'flymake-allowed-file-name-masks '("\\.module$" flymake-php-init))
+(add-to-list 'flymake-allowed-file-name-masks '("\\.install$" flymake-php-init))
+(add-to-list 'flymake-allowed-file-name-masks '("\\.inc$" flymake-php-init))
+(add-to-list 'flymake-allowed-file-name-masks '("\\.engine$" flymake-php-init))
