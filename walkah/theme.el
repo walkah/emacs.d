826da9bc35dfacc249cf62f351684ab331aa3113
James Walker <walkah@walkah.net>
2012-01-28 23:45:36 -0500

    handle the new magit faces a little better

--- walkah/theme.el
+++ walkah/theme.el
@@ -7,7 +7,11 @@
 
 (custom-set-faces
  '(flymake-errline ((t :underline "red")))
- '(flymake-warnline ((t :underline "green"))))
+ '(flymake-warnline ((t :underline "green")))
+ '(magit-header ((t :weight bold)))
+ '(magit-diff-add ((t :foreground "green")))
+ '(magit-diff-del ((t :foreground "red")))) 
+
 
 ; Make inconsolata work on mac
 (when (featurep 'ns-win)
