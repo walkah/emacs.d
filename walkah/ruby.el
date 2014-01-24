8357f0df3eb9508db41b52b0049716cec031e0ba
James Walker <walkah@walkah.net>
2014-01-24 15:29:10 -0500

    add .rake files for ruby-mode

--- walkah/ruby.el
+++ walkah/ruby.el
@@ -4,6 +4,8 @@
 (add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Capfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
 
 (add-hook 'ruby-mode-hook 'flymake-ruby-load)
 
